01c080bed434ea6a8d166b163adec4c7b11fd790
Raul Martin Catalan <rmartinc96@gmail.com>
2022-06-20 17:10:55 +0200

    First commit

---
+++ src/main/java/com/rdacompany/RDAsportsAPI/RdAsportsApplication.java
@@ -0,0 +1,13 @@
+package com.rdacompany.RDAsportsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdAsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdAsportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdacompany/RDAsportsAPI/RdAsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdacompany.RDAsportsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdAsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
